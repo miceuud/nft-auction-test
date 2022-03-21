a6a464d42da6d75b50afd050fd4df84b363a66e8
miceuud <miceuud@.gmail.com>
2022-03-21 01:27:54 +0100

    modified script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,23 @@
 const hre = require('hardhat')
 
 async function main() {
+
+  accounts = await ethers.getSigners()
+  signer = accounts[0]
+
   const NFTAuction = await hre.ethers.getContractFactory('NFTAuction')
-  const nftAuction = await NFTAuction.deploy()
-
+  const nftAuction = await NFTAuction.deploy(
+    // rinkeby dia contract address
+    '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa',
+  )
   await nftAuction.deployed()
-
-  console.log('auction', nftAuction.address)
 
   const MyNFT = await hre.ethers.getContractFactory('MyNFT')
   const myNft = await MyNFT.deploy(nftAuction.address)
-
   await myNft.deployed()
 
   console.log('nft: ', myNft.address)
+  console.log('auction: ', nftAuction.address)
 }
 main()
   .then(() => process.exit(0))
