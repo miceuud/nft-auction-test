47ad306050b51688c07bf19d83ebdb6554e6b0e4
miceuud <miceuud@.gmail.com>
2022-03-21 01:17:16 +0100

    Deleted sample text script

--- contracts/interfaces/IDaiToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-// Adding only the ERC-20 function we need
-interface IERCDaiToken {
-    function transfer(address _address, uint _amount) external returns (bool);
-    function transferFrom(address _src, address _destination, uint _amount) external;
-    function balanceOf(address _address) external view returns (uint);
-}
